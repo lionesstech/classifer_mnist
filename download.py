d8e1c456e34914f319de2b663334260bb6354154
lionesstech <52830273+lionesstech@users.noreply.github.com>
2025-02-28 09:16:37 +0200

    Add files via upload

--- download.py
+++ download.py
@@ -1,11 +1,13 @@
 from tensorflow.keras.datasets import mnist
 import pickle
-
+import matplotlib.pyplot as plt
+import random,cv2
 
 #  dump obj to pickle
 def save_data(obj, name):
     with open(name, 'wb') as f:
         pickle.dump(obj, f, pickle.HIGHEST_PROTOCOL)
+
 
 # load obj from pickle
 def load_data(name):
@@ -16,6 +18,41 @@ def load_data(name):
 # load MNIST Data
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
+# create 4 backgrounds
+# measure there sizes
+shapes = []
+bgs = []
+for i in range(1, 5):
+    im = cv2.imread(f"bg{i}.jpg", cv2.IMREAD_GRAYSCALE)
+    plt.imshow(im, cmap='gray')
+    plt.axis('off')
+    #plt.show()
+    bgs.append(im)
+    print(im.shape)
+    shapes.append(im.shape)
+
+# create 1000 sub images that include 1 digit in random background
+odimgs = [] # object detection
+positions = [] #positions of digit
+ylabels=[]
+for i in range(1000):
+    random_bg = random.randint(0,3)
+    bg = bgs[random_bg].copy()
+    shape = shapes[random_bg]
+    random_x = random.randint(0, shape[0]-28-1)
+    random_y = random.randint(0,shape[1]-28-1)
+    random_index = random.randint(0, len(x_train)-1)
+    bg[random_x:(random_x+28),random_y:(random_y+28)] = x_train[random_index]
+    label = y_train[random_index]
+    plt.imshow(bg, cmap='gray')
+    plt.show()
+    odimgs.append(bg)
+    ylabels.append(label)
+    positions.append([random_x, random_y])
+    # print(random_x," ",random_y , " digit ",label)
+save_data((odimgs,positions,ylabels),'Object_detection.pkl')
+
+
 # normalized images
 x_train = x_train.astype('float32') / 255.
 x_train = x_train.reshape((len(x_train), -1))
@@ -25,3 +62,7 @@ x_test = x_test.reshape((len(x_test), -1))
 # save train and test data
 save_data((x_train, y_train), 'mnist_train.pkl')
 save_data((x_test, y_test), 'mnist_test.pkl')
+
+
+
+
--- train.py
+++ train.py
@@ -2,18 +2,23 @@ from tensorflow.keras.models import Model
 from tensorflow.keras.layers import Input, Dense, Dropout
 from tensorflow.keras.regularizers import l2
 from tensorflow.keras.optimizers import Adam
-from download import load_data
+import pickle
+
+# load obj from pickle
+def load_data(name):
+    with open(name, 'rb') as f:
+        return pickle.load(f)
 
 
 # build autoencoder for digit prediction
-def build_autoencoder(input_shape, dropout_rate=0.2, l2_reg=0.01):
+def build_autoencoder(input_shape, dropout_rate=0.2, l2_reg=0.01,numberofneurons=512):
 
     # conv image to 1-D
     input_img = Input(shape=(input_shape,))
 
     # Train relu network with 2 layers for encoding
-    encoded = Dense(512, activation='relu', kernel_regularizer=l2(l2_reg))(input_img)
-    encoded = Dense(512, activation='relu', kernel_regularizer=l2(l2_reg))(encoded)
+    encoded = Dense(numberofneurons, activation='relu', kernel_regularizer=l2(l2_reg))(input_img)
+    encoded = Dense(numberofneurons, activation='relu', kernel_regularizer=l2(l2_reg))(encoded)
     # regulation using Dropout
     encoded = Dropout(dropout_rate)(encoded)
 
@@ -30,6 +35,14 @@ def build_autoencoder(input_shape, dropout_rate=0.2, l2_reg=0.01):
 
 # load MNIST train data
 x_train, y_train = load_data('mnist_train.pkl')
+
+
+# build the object detection autoencoder
+autoencoder = build_autoencoder(x_train.shape[1],0.2,0.01,1000)
+autoencoder.fit(x_train, x_train, epochs=1000, batch_size=100, shuffle=True)
+model_path = f'Object_Detection.h5'
+autoencoder.save(model_path)
+exit()
 
 # for each digit 0-9 creat autoencoder
 for digit in range(10):
